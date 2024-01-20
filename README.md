6c907e82052433e67c466eff1650e78d051e345b
Leileisme <onlymymind1108@gmail.com>
2024-01-21 01:04:47 +0800

    繳交作業

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/IbyA1-oy)
+# BootstrapWeb與互動套件
+
+### 使用套件說明
+1. 灰搜尋列：Vue(搜尋icon收合)
+2. 導覽列：無
+3. 輪播圖：swiperjs(輪播)
+
+![01](https://raw.githubusercontent.com/Leileisme/Bootstrap_Web/main/images/readme%20img/01.jpg)
+
+4. banner：gsap右滑入
+
+![02](https://raw.githubusercontent.com/Leileisme/Bootstrap_Web/main/images/readme%20img/02.jpg)
+
+5. ourFarm：左區BS頁籤切換文字/gsap左滑入、右區swiperjs切換圖片/gsap右滑入
+
+![03](https://raw.githubusercontent.com/Leileisme/Bootstrap_Web/main/images/readme%20img/03.jpg)
+
+6. Featured_Offers：lightbox燈箱效果(hover有眼睛喔)
+
+![04](https://raw.githubusercontent.com/Leileisme/Bootstrap_Web/main/images/readme%20img/04.jpg)
+
+
+7. ourTeam：gsap下滑入
+
+![05](https://raw.githubusercontent.com/Leileisme/Bootstrap_Web/main/images/readme%20img/05.jpg)
+
+8. stay connected：gsap左滑入
+9. testimonial：gsap右滑入
+
+![06](https://raw.githubusercontent.com/Leileisme/Bootstrap_Web/main/images/readme%20img/06.jpg)
+
+10. our_blog：gsap上滑入、swiperjs(輪播)
+
+![07](https://raw.githubusercontent.com/Leileisme/Bootstrap_Web/main/images/readme%20img/07.jpg)
+
+11. logos：gsap右滑入
+12. connection：無
+13. footer:無
+![08](https://raw.githubusercontent.com/Leileisme/Bootstrap_Web/main/images/readme%20img/08.jpg)
+
---
+++ js/all_js.js
@@ -0,0 +1,180 @@
+   // 註冊 gsap plugin
+   gsap.registerPlugin(ScrollTrigger);
+
+   // gsap.from()
+   // 4.banner/icon
+   gsap.from('.banner_item', {
+     scrollTrigger: {
+     trigger: '.Carousel_box',
+     start: 'center',
+     end: 'top',
+     // markers: true, // 打開標記(看不到的觸發線)
+   },
+   x: 1000,
+   duration: 1.5,
+   opacity:0
+ })
+
+ // 5.ourFarm
+ gsap.from('.farm_text', {
+     scrollTrigger: {
+     trigger: '.banner_item',
+     start: 'center',
+     end: 'top',
+     // markers: true, // 打開標記(看不到的觸發線)
+   },
+   x: -1000,
+   duration: 1.5,
+   opacity:0
+ })
+
+ gsap.from('.farm_img', {
+     scrollTrigger: {
+     trigger: '.banner_item',
+     start: 'center',
+     end: 'top',
+     // markers: true, // 打開標記(看不到的觸發線)
+   },
+   x: 1000,
+   duration: 1.5,
+   opacity:0
+ })
+
+ // 7.ourTeam
+ gsap.from('.ourTeam_card', {
+ scrollTrigger: {
+ trigger: '.Featured_Offers_box',
+ start: 'center',
+ end: 'top',
+ // markers: true, // 打開標記(看不到的觸發線)
+   },
+   y: 1000,
+   duration: 1.5,
+   opacity:0
+ })
+
+ // 8.connected
+ gsap.from('.connected_box', {
+ scrollTrigger: {
+ trigger: '.ourTeam_box',
+ start: 'center ',
+ end: 'center ',
+ // markers: true, // 打開標記(看不到的觸發線)
+   },
+   x: -2000,
+   duration: 1.2,
+ })
+
+ // 9.testimonial
+ gsap.from('.carousel02_box', {
+ scrollTrigger: {
+ trigger: '.ourTeam_box',
+ start: '70% top',
+ end: 'center center',
+//  markers: true, // 打開標記(看不到的觸發線)
+   },
+   x: 2000,
+   duration: 1.2,
+ })
+
+ // 10.our blog
+ gsap.from('.ourBlog_box_box', {
+ scrollTrigger: {
+ trigger: '.connected_box',
+ start: 'top',
+ end: 'top',
+ // markers: true, // 打開標記(看不到的觸發線)
+   },
+   y: -1500,
+   duration: 1.5,
+   opacity:0
+ })
+
+ // 11.logos
+ gsap.from('.logos_box', {
+ scrollTrigger: {
+ trigger: '.ourBlog_box_box',
+ start: 'bottom',
+ end: 'top',
+//  markers: true, // 打開標記(看不到的觸發線)
+   },
+   x: 2000,
+   duration: 2,
+   delay: 1, 
+ })
+
+   // swiper 輪播圖
+   var swiper = new Swiper('.mySwiperCarousel', {
+     loop: true,
+     effect: 'fade', // 使用淡入淡出效果
+     // autoplay: {
+     //   delay: 500000, // 切換間隔時間 (毫秒)
+     //   disableOnInteraction: false // 使用者互動後是否停止自動播放
+     // },
+     navigation: {
+       nextEl: '.swiper-button-next',
+       prevEl: '.swiper-button-prev'
+     }
+   })
+
+   // 第五部分 導覽
+   $('.nav-link').on('shown.bs.tab', function (e) {
+     // 移除所有 nav-link 的文字顏色
+     $('.nav-link').removeClass('farm_active_text')
+
+     // 將點擊的 nav-link 添加文字綠色樣式
+     $(this).addClass('farm_active_text')
+   })
+
+   // 第五部分 swiper  Thumbs gallery
+   var swiperThumbs = new Swiper('.mySwiper', {
+     loop: true,
+     spaceBetween: 10,
+     slidesPerView: 4,
+     freeMode: true,
+     watchSlidesProgress: true
+   })
+
+   var swiperGallery = new Swiper('.mySwiper2', {
+     loop: true,
+     spaceBetween: 10,
+     thumbs: {
+       swiper: swiperThumbs
+     }
+   })
+
+   swiperThumbs.on('click', function () {
+     var clickedIndex = swiperThumbs.clickedIndex
+     swiperGallery.slideTo(clickedIndex, 300, false)
+     updateThumbsBorder(clickedIndex)
+   })
+
+   function updateThumbsBorder(index) {
+     var thumbs = document.querySelectorAll('.mySwiper .swiper-slide')
+     // 移除所有綠色邊框
+     thumbs.forEach(function (thumb) {
+       thumb.classList.remove('green-border')
+     })
+     // 添加綠色邊框到點擊的小圖
+     thumbs[index].classList.add('green-border')
+   }
+
+   // 初始化時設置第一個小圖有綠色邊框
+   updateThumbsBorder(0)
+
+   // 第9區
+   var swiperCarousel2 = new Swiper('.mySwiperCarousel2', {
+     loop: true
+   })
+
+   // 第10區
+   var swiperCarousel2 = new Swiper('.mySwiper_ourBlog', {
+     loop: true,
+     breakpoints: {
+   576: {
+     slidesPerView: 2,
+   },
+   992: {
+     slidesPerView: 3, 
+   }
+   }})
